93f7bd96f463479b49aa9563c68aa78f0af1ecd7
Kristi Murphy <kristilmurphy@gmail.com>
2017-01-02 17:53:03 -0500

    commit 80% finished portfolio

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
 
-  // setInterval(function() {
-  //   $("#down-arrow").effect( "bounce", {times: 3}, 1000);
-  // }, 3000)
+  setInterval(function() {
+    $("#down-arrow").effect( "bounce", {times: 3}, 1000);
+  }, 3000)
+
+  $(".menu-wrapper").click(function(){
+    $(".fullscreen-menu").fadeToggle();
+    $("#menubutton").toggleClass("fa-bars hamburger-menu" || "fa-times menu-close");
+  });
+
+$("#menulink").click(function(){
+  $(".fullscreen-menu").toggle();
+  $(".menubutton").toggleClass("fa-bars hamburger-menu" || "fa-times menu-close");
+})
 
 
 
